a6494399abba2d62813aefb776ca272d80e7a192
EvoFox <foulkes.joshua@live.co.uk>
2022-07-04 16:37:37 +0100

    begin writing the routes

---
+++ src/user/routes.js
@@ -0,0 +1,7 @@
+const { Router } = require("express"); // Import Router method only from express
+const { signUp } = require("./controllers"); // Import controllers from ./controllers
+const userRouter = Router(); // Create a Router that can have endpoints added to it.
+
+userRouter.post("/user", signUp); // define a post request on /user path that calls the signUp controller
+
+module.exports = userRouter;
