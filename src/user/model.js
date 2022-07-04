a6c44d9c23c085e76d56ff81187e29908cfd87f8
EvoFox <foulkes.joshua@live.co.uk>
2022-07-04 16:36:16 +0100

    define model/schema

---
+++ src/user/model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose"); // Import mongoose
+
+// Define the users schema
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		require: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+		require: true,
+	},
+	pass: {
+		type: String,
+		required: true,
+	},
+});
+
+// Map the schema to a model
+const User = mongoose.model("User", userSchema);
+
+// export the model
+module.exports = User;
