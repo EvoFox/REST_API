d7c4aad95ef589bc8ce02618ccc1e9ff543af6e1
EvoFox <foulkes.joshua@live.co.uk>
2022-07-05 14:29:23 +0100

    modified login route to use POST for security

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express"); // Import Router method only from express
-const { signUp, logIn, searchUser } = require("./controllers"); // Import controllers from ./controllers
+const { signUp, login, searchUser } = require("./controllers"); // Import controllers from ./controllers
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
 userRouter.post("/user", signUp); // define a post request on /user endpoint that calls the signUp controller
-userRouter.get("/log-in", logIn); // define a get request on /log-in endpoint that calls the logIn controller
+userRouter.post("/login", login); // define a get request on /log-in endpoint that calls the logIn controller
 userRouter.get("/search-user", searchUser); // define a get request on /search-user endpoint that calls the searchUser controller
 
 module.exports = { userRouter };
