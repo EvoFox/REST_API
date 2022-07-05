996460d7bb7d755d6365a0561be58ee70cacb07f
EvoFox <foulkes.joshua@live.co.uk>
2022-07-05 11:51:27 +0100

    created endpoint for searching usernames

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express"); // Import Router method only from express
-const { signUp, logIn } = require("./controllers"); // Import controllers from ./controllers
+const { signUp, logIn, searchUser } = require("./controllers"); // Import controllers from ./controllers
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
 userRouter.post("/user", signUp); // define a post request on /user path that calls the signUp controller
 userRouter.get("/log-in", logIn);
+userRouter.get("/search-user", searchUser);
 
 module.exports = { userRouter };
