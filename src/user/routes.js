f0ba967a5e439dd906392180d887469bb6732372
EvoFox <foulkes.joshua@live.co.uk>
2022-07-05 14:18:14 +0100

    add comments

--- src/user/routes.js
+++ src/user/routes.js
@@ -2,8 +2,8 @@ const { Router } = require("express"); // Import Router method only from express
 const { signUp, logIn, searchUser } = require("./controllers"); // Import controllers from ./controllers
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
-userRouter.post("/user", signUp); // define a post request on /user path that calls the signUp controller
-userRouter.get("/log-in", logIn);
-userRouter.get("/search-user", searchUser);
+userRouter.post("/user", signUp); // define a post request on /user endpoint that calls the signUp controller
+userRouter.get("/log-in", logIn); // define a get request on /log-in endpoint that calls the logIn controller
+userRouter.get("/search-user", searchUser); // define a get request on /search-user endpoint that calls the searchUser controller
 
 module.exports = { userRouter };
