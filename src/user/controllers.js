cb426a29f457231e20979ebefe142a88e54539ed
EvoFox <foulkes.joshua@live.co.uk>
2022-07-05 11:51:06 +0100

    add comments, add username search that allows for partial usernames as criteria

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -4,6 +4,7 @@ const getToken = () => {
 	return "placeholder";
 };
 
+// Create user
 exports.signUp = async (req, res) => {
 	try {
 		// req.body contains k/v pairs that match the User model
@@ -15,12 +16,12 @@ exports.signUp = async (req, res) => {
 	}
 };
 
+// Read/Check if user exists
 exports.logIn = async (req, res) => {
 	try {
 		// Search the database for the user provided email and password
 		const existingUser = await User.find(req.body);
 
-		console.log(existingUser);
 		// Check the length of results
 		if (!existingUser.length) {
 			// If existingUser is EMPTY:
@@ -31,6 +32,22 @@ exports.logIn = async (req, res) => {
 			console.log("Welcome back");
 			res.send({ success: true, session_token: getToken() });
 		}
+	} catch (error) {
+		console.log(error);
+		res.send({ error });
+	}
+};
+
+exports.searchUser = async (req, res) => {
+	try {
+		// req input = { "username": "<username>" }
+		const re = new RegExp(`${req.body.username}`); // Define regex pattern to search usernames based on partial string
+		const details = await User.find({ username: { $regex: re } }); // Search database on usernames using Regex pattern, storing results in details
+
+		let arrResult = []; // Define an empty array to store ONLY usernames in
+		details.map((i) => arrResult.push(i.username)); // Map the results of the query to arrResult in order to form the response.
+
+		res.send({ username: arrResult }); // Send the formulated response.
 	} catch (error) {
 		console.log(error);
 		res.send({ error });
