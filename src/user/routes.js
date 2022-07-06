f1c31535e754497da2285a6b861a16b24a1b78b4
EvoFox <foulkes.joshua@live.co.uk>
2022-07-06 15:08:06 +0100

    add changePass endpoint

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express"); // Import Router method only from express
-const { signUp, login, searchUser } = require("./controllers"); // Import controllers from ./controllers
+const { signUp, login, searchUser, changePassword } = require("./controllers"); // Import controllers from ./controllers
 const { hashPass, comparePass } = require("../middleware"); // Import middleware from ../middleware
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
 userRouter.post("/user", hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
 userRouter.post("/login", comparePass, login); // define a get request on /log-in endpoint that calls the logIn controller
+userRouter.post("/change-password", comparePass, hashPass, changePassword);
 userRouter.get("/search-user", searchUser); // define a get request on /search-user endpoint that calls the searchUser controller
 
 module.exports = { userRouter };
