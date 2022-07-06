29eebfd609d41b52adb39a613183057a91e68ff9
EvoFox <foulkes.joshua@live.co.uk>
2022-07-06 15:57:14 +0100

    add comments, reorganise endpoints

--- src/user/routes.js
+++ src/user/routes.js
@@ -9,10 +9,17 @@ const {
 const { hashPass, comparePass } = require("../middleware"); // Import middleware from ../middleware
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
+// Create
 userRouter.post("/user", hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
+
+// Read
 userRouter.post("/login", comparePass, login); // define a get request on /log-in endpoint that calls the logIn controller
-userRouter.put("/change-password", comparePass, hashPass, changePassword); // define a post request on /change-password endpoint that calls the changePassword controller
-userRouter.delete("/delete-account", comparePass, hashPass, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
 userRouter.get("/search-user", searchUser); // define a get request on /search-user endpoint that calls the searchUser controller
+
+// Update 
+userRouter.put("/change-password", comparePass, hashPass, changePassword); // define a post request on /change-password endpoint that calls the changePassword controller
+
+// Delete
+userRouter.delete("/delete-account", comparePass, hashPass, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
 
 module.exports = { userRouter }; // export the Router so that it can be used in the server
