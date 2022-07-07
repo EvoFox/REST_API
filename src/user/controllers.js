a6a179738d939d81c0a57ab447fe5f64a8dbe96a
EvoFox <foulkes.joshua@live.co.uk>
2022-07-07 13:38:42 +0100

    removed User Search method, and implemented JWT's

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -1,15 +1,14 @@
+const jwt = require("jsonwebtoken");
 const User = require("./model");
-
-const getToken = () => {
-	return "placeholder";
-};
+require("dotenv").config();
 
 // Create user
 exports.signUp = async (req, res) => {
 	try {
 		// req.body contains k/v pairs that match the User model
 		const newUser = await User.create(req.body);
-		res.send({ user: newUser });
+		const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY);
+		res.send({ user: newUser, token });
 	} catch (error) {
 		console.log(error);
 		res.send({ error });
@@ -19,29 +18,13 @@ exports.signUp = async (req, res) => {
 // Read/Check if user exists
 exports.login = async (req, res) => {
 	try {
-		if (req.body.auth) {
-			// auth succeeded
-			console.log(`${req.body.username} has logged in`);
-			res.send({ user: req.body.username, session_token: "placeholder" });
-		} else {
+		if (!req.user) {
 			throw new Error("Invalid credentials");
+		} else {
+			// auth succeeded
+			console.log(`${req.user.username} has logged in`);
+			res.send({ user: req.user.username });
 		}
-	} catch (error) {
-		console.log(error);
-		res.send({ error });
-	}
-};
-
-exports.searchUser = async (req, res) => {
-	try {
-		// req input = { "username": "<username>" }
-		const re = new RegExp(`${req.body.username}`); // Define regex pattern to search usernames based on partial string
-		const details = await User.find({ username: { $regex: re } }); // Search database on usernames using Regex pattern, storing results in details
-
-		let arrResult = []; // Define an empty array to store ONLY usernames in
-		details.map((i) => arrResult.push(i.username)); // Map the results of the query to arrResult in order to form the response.
-
-		res.send({ username: arrResult }); // Send the formulated response.
 	} catch (error) {
 		console.log(error);
 		res.send({ error });
@@ -51,16 +34,16 @@ exports.searchUser = async (req, res) => {
 exports.changePassword = async (req, res) => {
 	try {
 		// Check if the middleware has authenticated the request based on current password
-		if (req.body.auth) {
+		if (!req.user) {
+			throw new Error("Invalid credentials");
+		} else {
 			// search database for record matching provided request body, store it to variable
 			// Then set the password hashed by the middleware
 			const user = await User.updateOne(
-				{ $or: [{ username: req.body.username }, { email: req.body.email }] },
-				{ pass: req.body.newPass }
+				{ username: req.body.username },
+				{ pass: req.user.newPass }
 			);
 			res.send({ user });
-		} else {
-			throw new Error("Invalid credentials");
 		}
 		// send the result of the update command
 	} catch (error) {
@@ -71,13 +54,13 @@ exports.changePassword = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
 	try {
-		if (req.body.auth) {
+		if (!req.user) {
+			throw new Error("Invalid credentials");
+		} else {
 			const user = await User.deleteOne({
-				$or: [{ username: req.body.username }, { email: req.body.email }],
+				$and: [{ username: req.user.username }, { email: req.user.email }],
 			});
 			res.send({ user });
-		} else {
-			throw new Error("Invalid credentials");
 		}
 	} catch (error) {
 		console.log(error);
