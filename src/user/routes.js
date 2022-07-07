0268f1f5955dfd731b3b798068c3d0c8c2c30962
EvoFox <foulkes.joshua@live.co.uk>
2022-07-07 13:39:59 +0100

    Reorganised Routes, implemented tokenCheck middleware on new route and removed searchUser route

--- src/user/routes.js
+++ src/user/routes.js
@@ -2,24 +2,20 @@ const { Router } = require("express"); // Import Router method only from express
 const {
 	signUp,
 	login,
-	searchUser,
 	changePassword,
 	deleteUser,
 } = require("./controllers"); // Import controllers from ./controllers
-const { hashPass, comparePass } = require("../middleware"); // Import middleware from ../middleware
+const { hashPass, comparePass, tokenCheck } = require("../middleware"); // Import middleware from ../middleware
 const userRouter = Router(); // Create a Router that can have endpoints added to it.
 
-// Create
+
 userRouter.post("/user", hashPass, signUp); // define a post request on /user endpoint that calls the signUp controller
+userRouter.get("/user", tokenCheck, login) // define a get request on /user endpoint that calls the tokenCheck and login methods
 
-// Read
 userRouter.post("/login", comparePass, login); // define a get request on /log-in endpoint that calls the logIn controller
-userRouter.get("/search-user", searchUser); // define a get request on /search-user endpoint that calls the searchUser controller
 
-// Update 
 userRouter.put("/change-password", comparePass, hashPass, changePassword); // define a post request on /change-password endpoint that calls the changePassword controller
 
-// Delete
-userRouter.delete("/delete-account", comparePass, hashPass, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
+userRouter.delete("/delete-account", comparePass, deleteUser); // define a delete request on /delete-account endpoint that calls the deleteUser controller
 
 module.exports = { userRouter }; // export the Router so that it can be used in the server
