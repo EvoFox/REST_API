bd5e65d71564b17d4afe6d903c2406989e6594cd
EvoFox <foulkes.joshua@live.co.uk>
2022-07-07 13:37:54 +0100

    Implement JSON Web Tokens and streamlined the code

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,9 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const User = require("../user/model");
+require("dotenv").config();
+
 const _SALT = 8;
+const User = require("../user/model");
 
 exports.hashPass = async (req, res, next) => {
 	try {
@@ -8,7 +11,7 @@ exports.hashPass = async (req, res, next) => {
 
 		// If changing password:
 		if (req.body.newPass) {
-			req.body.newPass = await bcrypt.hash(req.body.newPass, _SALT); // Hash the password from req.body.newPass if it exists, reassert into req.body.newPass
+			req.user.newPass = await bcrypt.hash(req.body.newPass, _SALT); // Hash the password from req.body.newPass if it exists, reassert into req.body.newPass
 		}
 
 		next(); // Moves onto next middleware/controller in endpoint
@@ -20,27 +23,33 @@ exports.hashPass = async (req, res, next) => {
 
 exports.comparePass = async (req, res, next) => {
 	try {
-		// Lookup username OR email in database - This allows it to be used in functions that require either of them for searching
-		const user = await User.findOne({
-			$or: [{ username: req.body.username }, { email: req.body.email }],
-		});
+		// Lookup username OR email in database - Store it in req.user
+		req.user = await User.findOne({ username: req.body.username });
 
-		// Check if a record exists in the database matching criteria
-		if (!user) {
-			// If a record is not found, throw a new error "Account not found"
-			throw new Error("Account not found.");
+		// Check the req.user password matches the req.body password
+		if (await bcrypt.compare(req.body.pass, req.user.pass)) {
+			next(); // Move onto the next middleware/controller in endpoint
 		} else {
-			// Check if the passwords match
-			if (await await bcrypt.compare(req.body.pass, user.pass)) {
-				// If they match, add auth:true to request body
-				req.body.auth = true;
-			} else {
-				// If they do not match, throw a new error "Invalid login details"
-				throw new Error("Invalid login details");
-			}
+			// If they do not match, throw a new error "Invalid login details"
+			throw new Error("Invalid login details");
 		}
+	} catch (error) {
+		console.log(error);
+		res.send({ error });
+	}
+};
 
-		next(); // Move onto the next middleware/controller in endpoint
+exports.tokenCheck = async (req, res, next) => {
+	try {
+		// Decode token using secret key stored in .env file
+		const decodedToken = jwt.verify(
+			req.header("Authorization"),
+			process.env.SECRET_KEY
+		);
+
+		// Find user by their ID stored in the token
+		req.user = User.findById(decodedToken.id);
+		next();
 	} catch (error) {
 		console.log(error);
 		res.send({ error });
